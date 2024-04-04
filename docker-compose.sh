feeb7444c70a28701145f6c4bc20a93739b229a2
Your Name <you@example.com>
2024-04-04 13:56:07 +0000

    Update Samba file names with random numbers

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,6 +4,7 @@ set -xeo pipefail
 which docker-compose || apt-get install -y docker-compose
 which kompose || (curl -L https://github.com/kubernetes/kompose/releases/download/v1.32.0/kompose-linux-amd64 -o kompose && chmod +x kompose && mv kompose /usr/local/bin/kompose)
 
+export RANDOM=`date +%s`
 kompose convert -f ./docker-compose.yml
 FILE="samba-service.yaml samba-deployment.yaml samba-claim0-persistentvolumeclaim.yaml"
 for f in $FILE; do
