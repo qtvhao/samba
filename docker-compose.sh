5589ade4ce8d1d47d4b57ea71221237b6bf4c19c
Your Name <you@example.com>
2024-04-04 07:02:58 +0000

    Update Docker Compose and Kubernetes configuration

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,6 +4,10 @@ set -xeo pipefail
 which docker-compose || apt-get install -y docker-compose
 which kompose || (curl -L https://github.com/kubernetes/kompose/releases/download/v1.32.0/kompose-linux-amd64 -o kompose && chmod +x kompose && mv kompose /usr/local/bin/kompose)
 
-kompose convert -f ./docker-compose.yml
-docker-compose up -d
-docker-compose logs -f
+# kompose convert -f ./docker-compose.yml
+FILE="samba-service.yaml samba-pod.yaml samba-claim0-persistentvolumeclaim.yaml samba-client-pod.yaml"
+for f in $FILE; do
+    kubectl apply -f $f
+done
+# docker-compose up -d
+# docker-compose logs -f
