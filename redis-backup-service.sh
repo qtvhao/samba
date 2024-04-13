a49d3b9d1ea93ce0d9f6a8ca3a6d6981ef4554c3
Your Name <you@example.com>
2024-04-13 02:07:27 +0000

    Refactor Redis backup service script to handle database restoration and backup

--- redis-backup-service.sh
+++ redis-backup-service.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -xeo pipefail
 
-_term() { 
+_term() {
     echo "Caught SIGTERM signal! It means that the container is being stopped. Saving the database...";
     redis-cli -a $REDIS_PASSWORD -h redis set hello world;
     redis-cli -a $REDIS_PASSWORD -h localhost save;
-    sleep 8
     cp /bitnami/redis/data/dump.rdb /bitnami/redis/data/backup-service/dump.rdb
     kill -TERM "$child"
 }
@@ -13,9 +12,16 @@ _term() {
 trap _term SIGTERM
 
 echo "Doing some initial work...";
-/opt/bitnami/scripts/redis/entrypoint.sh /opt/bitnami/scripts/redis/run.sh &
+if [ -f /bitnami/redis/data/backup-service/dump.rdb ]; then
+    cp /bitnami/redis/data/backup-service/dump.rdb /bitnami/redis/data/dump.rdb
+fi
+/opt/bitnami/scripts/redis/entrypoint.sh /opt/bitnami/scripts/redis/run.sh & child=$!
 sleep 2
 redis-cli -a $REDIS_PASSWORD -h redis get hello
-
-child=$! 
+HELLO=`redis-cli -a $REDIS_PASSWORD -h redis get hello`
+if [ "$HELLO" == "world" ]; then
+    echo "Database is restored successfully!";
+else
+    echo "Database is not restored successfully!";
+fi
 wait "$child"
